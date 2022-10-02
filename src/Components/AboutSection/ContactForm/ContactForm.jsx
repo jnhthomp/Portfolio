72d82a6e874bc7ef9d1250ed84accaa6fa7cf3e2
Jonah <jnh.thompson@gmail.com>
2022-10-02 01:14:25 -0700

    Add netlify property to contact form
    Should allow me to quickly store/receive any messages submitted without
    having to setup a mongodb/server to capture the request

--- src/Components/AboutSection/ContactForm/ContactForm.jsx
+++ src/Components/AboutSection/ContactForm/ContactForm.jsx
@@ -6,7 +6,7 @@ const ContactForm = (props) => {
   // TODO: Add nicer form features and custom form hook (see jtdev-component-library)
 
   return (
-    <form className={classes.contactForm} onSubmit={props.onFormSubmit}>
+    <form className={classes.contactForm} onSubmit={props.onFormSubmit} netlify>
       <div className={classes.formLine}>
         <div className={classes.inputGroup}>
           <label htmlFor='nameInput'>Your Name</label>
