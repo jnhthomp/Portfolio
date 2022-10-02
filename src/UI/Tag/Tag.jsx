8e8dc8193ffd0d6d8c0cec02182d2d3d7d2a63fa
Jonah <jnh.thompson@gmail.com>
2022-10-02 01:03:49 -0700

    Create tag component
    Will first be used to highlight skills used in projects but could be
    used elsewhere if necessary

---
+++ src/UI/Tag/Tag.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Tag.module.css'
+
+const Tag = (props) => {
+  return (
+    <div className={classes.tag}>{props.children}</div>
+  )
+}
+
+export default Tag
