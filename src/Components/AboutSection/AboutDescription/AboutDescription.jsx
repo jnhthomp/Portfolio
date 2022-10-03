ee4cd0bf25fe10a2c64bfdb945899326dcd431ad
Jonah <jnh.thompson@gmail.com>
2022-10-03 12:06:38 -0700

    Open resume in new tab instead of leaving page

--- src/Components/AboutSection/AboutDescription/AboutDescription.jsx
+++ src/Components/AboutSection/AboutDescription/AboutDescription.jsx
@@ -7,9 +7,8 @@ const AboutDescription = () => {
   // URL to hosted resume
   const resumeURL = 'https://jtdev-asset-server.herokuapp.com/assets/docs/JonahThompsonResume.pdf'; 
   const onResumeClick = () => {
-    // TODO: Add a fetch request here that updates a database to see how many times my resume was downloaded
-    // navigate to hosted resume on button click (if href somehow doesn't activate)
-    window.location.href = resumeURL
+    // TODO: Add a fetch request here that updates a database to see how many times my resume was downloaded and when
+    window.open(resumeURL, "_blank"); // navigate to hosted resume on button click
   }
 
   return (
@@ -23,7 +22,6 @@ const AboutDescription = () => {
       <p className={classes.selfDescription}>I am a web developer who loves solving real-world problems with code. I am used to fast-paced work environments having spent a large amount of my time in restaurants, retail, and a startup.</p>
 
       {/* <!-- Resume download button --> */}
-      {/* <!-- TODO: Add resume file to download --> */}
       <div className={classes.resumeBtnContainer}>
         <Button buttonAction={onResumeClick}><span className={classes.btnText}>My Resume</span></Button>
       </div>
