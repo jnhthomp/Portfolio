eee7ccc425e8ee2bf8d27a466346840469673d8e
Jonah <jnh.thompson@gmail.com>
2022-10-03 09:59:00 -0700

    Navbar mobile adjustments

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
+
+  const [showMobile, setShowMobile] = useState(true)
+
+  const toggleShowMobile = (e) => {
+    setShowMobile((prevState) => !prevState)
+  }
   return (
-    <div className={classes.navContainer}>
-      <ul>
+    <div className={`${classes.navContainer} ${showMobile ? classes.hasExpanded : ''}`}>
+      <div onClick={toggleShowMobile} className={classes.burger}>&#9776;</div>
+      <ul className={showMobile ? classes.expanded : ''}>
         <li className={classes.active}>Home</li>
         <li>Projects</li>
         <li>Experience</li>
